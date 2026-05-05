c3ef338bef0251813bbb33b571a984dfa87f0318
josehuesca89 <josehuesca89@gmail.com>
2026-05-05 13:33:48 -0400

    Preserve base path in vite.config.ts
    Retain the base path for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Use 'plugin-react' here
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GluteSynk/',
+  base: '/GluteSynk/', // This line is the most important!
 })
