2601bb35c64eb37fb9193093890399c0ce8248af
josehuesca89 <josehuesca89@gmail.com>
2026-05-05 12:16:40 -0400

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GluteSynk/', 
+  base: '/GluteSynk/',
 })
