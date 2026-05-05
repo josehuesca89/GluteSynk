eb81a55b5d76f0a8d11930a07632b3828ddae6a9
josehuesca89 <josehuesca89@gmail.com>
2026-05-05 13:23:14 -0400

    Refactor imports in main.tsx for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // <--- Make sure this line exists!
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
